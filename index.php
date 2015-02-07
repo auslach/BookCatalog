dbe89bb8579fd187ce253d1a94b322528f1b26a6
Austin L. Chang <austinlchang@gmail.com>
2015-02-06 17:07:09 -0800

    working pagination

--- index.php
+++ index.php
@@ -2,26 +2,70 @@
 <html lang="en-US">
   <head>
     <title>Book Catalog</title>
+  <style>
+    table, td, tr, td {
+      border: 1px solid black;
+    }
+  </style>
   </head>
   <body>
-  <?php 
+  <?php
   	$dsn = 'mysql:host=localhost;dbname=BookCatalog';
   	$username = 'php';
   	$password = 'php';
 
   	$db = new PDO($dsn, $username, $password);
+    // ============================
 
-  	$query = 'SELECT course.courseID, course.courseTitle, book.isbn13, book.bookTitle, book.price
+    $limit = 6;
+    $adjacents = 3;
+
+    $page = $_GET['page'];
+    if ($page) {
+      $start = ($page - 1) * $limit;
+    } else {
+      $start = 0;
+    }
+
+  	$select = "SELECT course.courseID, course.courseTitle, book.isbn13, book.bookTitle, book.price
 							FROM book
 							JOIN coursebook
 							ON book.isbn13=coursebook.book
 							JOIN course
 							ON coursebook.course=course.courseID
-							ORDER BY course.courseID';
+							ORDER BY course.courseID";
+    $query = $select." LIMIT $start, $limit";
 
   	$catalog = $db->query($query);
-  ?>
+    $count = ceil($db->query($select)->rowCount() / $limit);
 
+    if ($page == 0) $page = 1; // if no page variable, default to 1
+    $prev = $page - 1; // prev, subtract 1 from page
+    $next = $page + 1; // next, add 1 to page
+    $lastpage = ceil($total_pages/$limit);
+
+  ?>
+  <br />
+  pagination
+  <br />
+
+  <table>
+    <tr>
+      <td>
+        <?php if ($page > 1) : ?>
+          <a href="<?php echo "?page=$prev"; ?>"><?php echo $prev; ?></a>
+        <?php endif; ?>
+      </td>
+      <td>
+        <?php echo $page; ?>
+      </td>
+      <td>
+        <?php if ($count > $page) : ?>
+          <a href="<?php echo "?page=$next"; ?>"><?php echo $next; ?></a>
+        <?php endif; ?>
+      </td>
+    </tr>
+  </table>
   <table>
   	<th>
   		Course #
@@ -56,7 +100,7 @@
 			  		<?php echo $book['bookTitle']; ?>
 		  		</td>
 		  		<td>
-				  	<?php echo $book['price']; ?>
+				  	$<?php echo $book['price']; ?>
 		  		</td>
 		  		<td>
 		  			Add to cart
